76891324122aa97a716cb80d124da6adb4b86457
teixeiras <teixeiras@gmail.com>
2015-05-24 16:26:58 +0100

    %Updated new services

--- server/static/plugin/modules/configs.js
+++ server/static/plugin/modules/configs.js
@@ -4,7 +4,7 @@ function Config () {
 
 Config.prototype.getWebsocketAddress = function () {
     if (window.chrome && chrome.runtime && chrome.runtime.id) {
-        return 'ws://'+location.hostname+':9090/echo';
+        return 'ws://'+location.hostname+':9595/echo';
     } else  {
         return 'ws://10.211.55.6:9090/echo';
     }
