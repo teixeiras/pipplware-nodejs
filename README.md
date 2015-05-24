3b44814f1d8e3d7114f924379ae8857e1ca54853
teixeiras <teixeiras@gmail.com>
2015-05-24 15:34:17 +0100

    %Updated new services

---
+++ README.md
@@ -0,0 +1,4 @@
+
+To install execute this at rpi:
+
+bash <(curl -s https://raw.githubusercontent.com/teixeiras/pipplware-nodejs/master/install.sh)
--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 mkdir ~/pipplware
 cd ~/pipplware
+sudo apt-get -y install libavahi-compat-libdnssd-dev
+
+wget http://node-arm.herokuapp.com/node_latest_armhf.deb
+sudo dpkg -i node_latest_armhf.deb
+rm node_latest_armhf.deb
+
+
+git clone https://github.com/teixeiras/pipplware-nodejs
+cd pipplware-nodejs/server
+npm i
