0c201ffd24fe43b7453a91d8957effae69e7af67
teixeiras <teixeiras@gmail.com>
2015-06-12 22:23:37 +0100

    %Styling

---
+++ build.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+set -u
+
+set -e
+
+HOME=/home/pi/jenkins/pipplware-server
+
+cd $HOME
+
+DIST=${HOME}/debinstall/deb-src
+
+SYSROOT=${DIST}/sysroot
+
+DEBIAN=${DIST}/DEBIAN
+
+VERSION=0.1
+
+sudo rm -rf $DIST
+
+mkdir -p $DIST
+
+mkdir -p $SYSROOT
+
+mkdir -p $DEBIAN
+
+mkdir -p $SYSROOT/etc/udev/rules.d/
+
+echo 'KERNEL=="uinput", MODE="0666"' >  $DIST/sysroot/etc/udev/rules.d/10-udev.rules
+
+sudo apt-get -y install libavahi-compat-libdnssd-dev
+
+wget http://node-arm.herokuapp.com/node_latest_armhf.deb
+
+sudo DEBIAN_FRONTEND=noninteractive dpkg -i node_latest_armhf.deb
+
+#git clone https://github.com/teixeiras/pipplware-nodejs
+
+pwd
+
+ls
+
+cd server
+
+npm i
+
+
+mkdir -p $SYSROOT/opt/pipplware-server
+
+cp -r ./* $SYSROOT/opt/pipplware-server
+
+cp -r $HOME/DEBIAN/*  $DEBIAN
+
+
+find ${DIST}/ -type d -exec sudo chmod 0755 {} \;
+
+find ${DIST}/ -type f -exec sudo chmod go-w {} \;
+
+sudo chown -R root:root ${DIST}/
+
+let SIZE=`sudo du -s ${SYSROOT} | sed s'/\s\+.*//'`+8
+
+pushd ${SYSROOT}/
+
+sudo tar czf ${DIST}/data.tar.gz [a-z]*
+
+popd
+
+sudo sed s"/SIZE/${SIZE}/" -i ${DEBIAN}/control
+
+pushd ${DEBIAN}
+
+sudo tar czf ${DIST}/control.tar.gz *
+
+popd
+
+pushd ${DIST}/
+
+echo 2.0 > ./debian-binary
+
+find ${DIST}/ -type d -exec sudo chmod 0755 {} \;
+
+find ${DIST}/ -type f -exec sudo chmod go-w {} \;
+
+sudo chown -R root:root ${DIST}/
+
+sudo ar r $HOME/pipplware-server_$VERSION.deb debian-binary control.tar.gz data.tar.gz
+
+popd
+rm -rf  $HOME/deb_dist
+mkdir $HOME/deb_dist
+
+cp $HOME/node_latest_armhf.deb $HOME/deb_dist
+
+cp $HOME/pipplware-server_$VERSION.deb $HOME/deb_dist
